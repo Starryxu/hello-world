969e660e7569e072c8f49e28707fd77f315919d8
Longbo Ma <mlongbo@gmail.com>
2016-04-07 00:44:13 +0800

    Hello Java

---
+++ java/HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+ * Print "Hello World" on screen
+ * @auth Longbo Ma
+ */
+public class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
