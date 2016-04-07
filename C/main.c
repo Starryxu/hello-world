6e8f410caf192341f065f4628d6b24248ffa7bbb
Longbo Ma <mlongbo@gmail.com>
2016-04-07 10:49:05 +0800

    update comment

--- C/main.c
+++ C/main.c
@@ -1,11 +1,4 @@
-//
-//  main.c
-//  Hello
-//
-//  Created by tangkunyin on 16/4/7.
-//  Copyright © 2016年 www.shuoit.net. All rights reserved.
-//
-
+// C Hello World
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
